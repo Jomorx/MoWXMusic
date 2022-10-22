62eaf8ad6c7d81a4e5ce24a80770584238aa2328
huchanghao <2955568032@qq.com>
2022-10-22 16:16:54 +0800

    完成巅峰榜单

--- pages/home-music/index.js
+++ pages/home-music/index.js
@@ -1,11 +1,14 @@
 // pages/home-music/index.js
 import {
-    getBanners, getSongMenu
+    getBanners,
+    getSongMenu
 } from '../../service/api_music'
-import {rankingStore} from '../../store/index'
+import {
+    rankingStore
+} from '../../store/index'
 import queryRect from '../../utils/query-rect'
 import throttle from '../../utils/throttle'
-const throttleQueryRect = throttle(queryRect,1000)
+const throttleQueryRect = throttle(queryRect, 1000)
 Page({
 
     /**
@@ -13,20 +16,31 @@ Page({
      */
     data: {
         banners: [],
-        swiperHeight:0,
-        recommendSongs:[],
-        hotSongMenu:[],
-        recommendSongMenu:[]
+        swiperHeight: 0,
+        recommendSongs: [],
+        hotSongMenu: [],
+        recommendSongMenu: [],
+        rankings: {
+            0: {},
+            1: {},
+            3: {}
+        }
+
     },
     onLoad() {
         this.getPageData()
+        //发起共享数据请求
         rankingStore.dispatch("getRankingDataAction")
-        rankingStore.onState("hotRanking",res=>{
-            if(!res.tracks) return 
-            const recommendSongs = res.tracks.slice(0,6)
-            // console.log(recommendSongs);
-            this.setData({recommendSongs})
+        rankingStore.onState("hotRanking", res => {
+            if (!res.tracks) return
+            const recommendSongs = res.tracks.slice(0, 6)
+            this.setData({
+                recommendSongs
+            })
         })
+        rankingStore.onState("newRanking", this.getRankingHandler(0))
+        rankingStore.onState("hotRanking", this.getRankingHandler(1))
+        rankingStore.onState("upRanking", this.getRankingHandler(3))
     },
     handleSearchClick() {
         wx.navigateTo({
@@ -39,18 +53,46 @@ Page({
                 banners: res.banners
             })
         })
-        getSongMenu("全部").then(res=>{
+        getSongMenu("全部").then(res => {
             // console.log(res);
-            this.setData({hotSongMenu:res.playlists})
+            this.setData({
+                hotSongMenu: res.playlists
+            })
         })
-        getSongMenu("日语").then(res=>{
+        getSongMenu("日语").then(res => {
             // console.log(res);
-            this.setData({recommendSongMenu:res.playlists})
+            this.setData({
+                recommendSongMenu: res.playlists
+            })
         })
 
     },
-   async handleSwiperImageLoaded() {
-    const rect= await  throttleQueryRect(".swiper-image")
-      this.setData({swiperHeight:rect[0].height})
-    }
+    async handleSwiperImageLoaded() {
+        const rect = await throttleQueryRect(".swiper-image")
+        this.setData({
+            swiperHeight: rect[0].height
+        })
+    },
+    getRankingHandler: function (idx) {
+        return (res) => {
+            if (Object.keys(res).length === 0) return
+            const name = res.name
+            const coverImgUrl = res.coverImgUrl
+            const playCount = res.playCount
+            const songList = res.tracks.slice(0, 3)
+            const rankingObj = {
+                name,
+                coverImgUrl,
+                playCount,
+                songList
+            }
+            this.setData({
+                rankings: {
+                    ...this.data.rankings,
+                    [idx]: rankingObj
+                }
+            })
+            console.log(this.data.rankings)
+        }
+    },
 })
--- store/ranking-store.js
+++ store/ranking-store.js
@@ -1,15 +1,26 @@
 import {HYEventStore} from 'hy-event-store';
 import {getRankings} from '../service/api_music'
+
+const rankingMap = { 0: "newRanking", 1: "hotRanking", 2: "originRanking", 3: "upRanking" }
+
 export const rankingStore = new HYEventStore({
     state:{
-        hotRanking:{}
+        newRanking: {}, // 0: 新歌
+        hotRanking: {}, // 1: 热门
+        originRanking: {}, // 2: 原创
+        upRanking: {} // 3: 飙升
     },
     actions:{
         getRankingDataAction(ctx){
-            getRankings(1).then(res=>{
-                // console.log(res);
-                ctx.hotRanking=res.playlist
-            })
+            // 0: 新歌榜 1: 热门榜 2: 原创榜 3: 飙升榜
+      for (let i = 0; i < 4; i++) {
+          if(i===2) continue;
+        getRankings(i).then(res => {
+          const rankingName = rankingMap[i]
+          ctx[rankingName] = res.playlist
+          console.log(res.playlist);
+        })
+      }
         }
     }
 })
